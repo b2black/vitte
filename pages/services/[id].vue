c200b7eeecafc90061d1cbdd59f17180e6109c0d
Bogdan Melkov <hellomelkov@gmail.com>
2025-05-15 22:00:27 +0300

    feat: вернул services/[id].vue

--- pages/services/[id].vue
+++ pages/services/[id].vue
@@ -36,39 +36,37 @@ useBreadcrumbItems({
     </div>
     <div class="py-8">
       <h1
-        class="text-2xl font-bold mb-8"
+          class="text-2xl font-bold mb-8"
       >
         {{ title }}
       </h1>
-      123123312
-      <ClientOnly>
-        <div
+
+      <div
           v-if="service?.image"
           class="image-container flex h-96 mb-8"
-        >
-          <NuxtImg
+      >
+        <NuxtImg
             :src="service?.image"
             class="w-full object-cover rounded-2xl"
             format="webp"
-          />
-        </div>
+        />
+      </div>
 
-        <div
+      <div
           v-if="service?.description"
           class="mb-8"
-        >
-          {{ service?.description }}
-        </div>
+      >
+        {{ service?.description }}
+      </div>
 
-        <div class="flex justify-end">
-          <UButton
+      <div class="flex justify-end">
+        <UButton
             variant="soft"
             @click="$router.back()"
-          >
-            Назад
-          </UButton>
-        </div>
-      </ClientOnly>
+        >
+          Назад
+        </UButton>
+      </div>
     </div>
   </main>
 </template>
